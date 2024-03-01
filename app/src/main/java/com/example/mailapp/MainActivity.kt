8904bdb4ec0ae968ba1061c9d076b9129b01584f
Brandon Kieng <brandonkieng182@gmail.com>
2024-02-29 16:02:29 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mailapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mailapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var emails: List<Email>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val emailRV = findViewById<RecyclerView>(R.id.rv_email)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailRV.adapter = adapter
+        // Set layout manager to position the items
+        emailRV.layoutManager = LinearLayoutManager(this)
+    }
+}
